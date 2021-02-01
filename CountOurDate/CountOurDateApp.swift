4e6cc013de24cc14c5ec3f9218de0e2fd18f3bf3
Wentao Xie <wentao.xie@everykey.com>
2021-01-31 19:05:03 -0500

    Initial Commit

---
+++ CountOurDate/CountOurDateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountOurDateApp.swift
+//  CountOurDate
+//
+//  Created by Sheldon on 1/31/21.
+//
+
+import SwiftUI
+
+@main
+struct CountOurDateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
